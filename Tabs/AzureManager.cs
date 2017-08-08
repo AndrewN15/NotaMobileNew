e36d1ad6f49c4b6b76cf33129f6aacc7fad34ef8
Andrew Nguyen <andy151195@hotmail.com>
2017-08-08 22:11:48 +1200

    Add project files and folders

---
+++ Tabs/AzureManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace Tabs
+{
+	public class AzureManager
+	{
+
+		private static AzureManager instance;
+		private MobileServiceClient client;
+		private IMobileServiceTable<NotaMobileModel> notMobileTable;
+
+		private AzureManager()
+		{
+			this.client = new MobileServiceClient("http://notamobile.azurewebsites.net");
+            this.notMobileTable = this.client.GetTable<NotaMobileModel>();
+		}
+
+		public MobileServiceClient AzureClient
+		{
+			get { return client; }
+		}
+
+		public static AzureManager AzureManagerInstance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new AzureManager();
+				}
+
+				return instance;
+			}
+		}
+
+		public async Task<List<NotaMobileModel>> GetMobileInformation()
+		{
+			return await this.notMobileTable.ToListAsync();
+		}
+
+        public async Task PostMobileInformation(NotaMobileModel notMobileModel)
+		{
+			await this.notMobileTable.InsertAsync(notMobileModel);
+		}
+	}
+}
---
+++ Tabs/AzureTable.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Tabs
+{
+    public partial class AzureTable : ContentPage
+    {
+		Geocoder geoCoder;
+       
+        public AzureTable()
+        {
+            InitializeComponent();
+            geoCoder = new Geocoder();
+
+		}
+
+		async void Handle_ClickedAsync(object sender, System.EventArgs e)
+		{
+            loading.IsRunning = true;
+			List<NotaMobileModel> notMobileInformation = await AzureManager.AzureManagerInstance.GetMobileInformation();
+
+			foreach (NotaMobileModel model in notMobileInformation)
+			{
+				var position = new Position(model.Latitude, model.Longitude);
+				var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(position);
+				foreach (var address in possibleAddresses)
+                    model.City = address;
+			}
+
+			MobileList.ItemsSource = notMobileInformation;
+            loading.IsRunning = false;
+		}
+
+    }
+}
